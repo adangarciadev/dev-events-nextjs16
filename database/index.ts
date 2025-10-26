3566db6f36346bbb8a66313d0e4db8560f37b241
Adan <adangg1988@gmail.com>
2025-10-26 22:11:08 +0100

    editing index file

--- database/index.ts
+++ database/index.ts
@@ -1,5 +1,7 @@
 // Exporta los modelos Event y Booking desde un solo archivo
-import Event from "./event.model";
-import Booking from "./booking.model";
+import { default as Event } from "./event.model";
+import { default as Booking } from "./booking.model";
 
-export { Event, Booking };
+// TypeScript interfaces exports
+export type { IEvent } from "./event.model";
+export type { IBooking } from "./booking.model";
